a73825a151e2c9b2f3fb0711ff662d913391f683
taryu <taryu1991@gmail.com>
2015-01-16 20:24:08 +0800

    lightfinish

--- app/main/views.py
+++ app/main/views.py
@@ -1,25 +1,25 @@
 from flask import render_template, url_for
 from . import main
 import RPi.GPIO as GPIO
-from ...manage import light
+import manage
 
 
 @main.route('/')
 def index():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(25, GPIO.OUT)
-    return render_template('index.html', light=light)
+    return render_template('index.html', light=manage.light)
 
 
 @main.route('/turnon')
 def turnon():
-    light = 1
+    manage.light = 1
     GPIO.output(25, GPIO.HIGH)
-    return render_template('index.html', light=light)
+    return render_template('index.html', light=manage.light)
 
 
 @main.route('/turnoff')
 def trunoff():
-    light = 0
+    manage.light = 0
     GPIO.output(25, GPIO.LOW)
-    return render_template('index.html', light=light)
+    return render_template('index.html', light=manage.light)
