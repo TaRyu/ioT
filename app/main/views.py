96c05239adb17d834044d158927649680f47c9fb
taryu <taryu1991@gmail.com>
2015-01-27 10:20:22 +0800

    温度感知webtest

--- app/main/views.py
+++ app/main/views.py
@@ -8,18 +8,18 @@ import manage
 def index():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(25, GPIO.OUT)
-    return render_template('index.html', light=manage.light)
+    return render_template('index.html', light=manage.light, temp=manage.temp)
 
 
 @main.route('/turnon')
 def turnon():
     manage.light = 1
     GPIO.output(25, GPIO.HIGH)
-    return render_template('index.html', light=manage.light)
+    return render_template('index.html', light=manage.light, temp=manage.temp)
 
 
 @main.route('/turnoff')
 def trunoff():
     manage.light = 0
     GPIO.output(25, GPIO.LOW)
-    return render_template('index.html', light=manage.light)
+    return render_template('index.html', light=manage.light, temp=manage.temp)
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 # coding: utf-8
-#!/usr/bin/env python
 import os
+import glob
+import time
 from app import create_app
 from flask.ext.script import Manager, Shell
 import RPi.GPIO as GPIO
@@ -8,6 +9,28 @@ import RPi.GPIO as GPIO
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 light = 0
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+
+def readTempRaw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def readTemp():
+    lines = readTempRaw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = readTempRaw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return round(temp_c, 1)
 
 
 def make_shell_context():
@@ -20,3 +43,8 @@ if __name__ == '__main__':
     GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(25, GPIO.OUT)
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+    while True:
+        temp = readTemp()
+        time.sleep(5)
